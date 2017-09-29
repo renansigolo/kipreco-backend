c66ad468cacde59bcf5e7edb4058406e5c3ec764
VinnyBuck <biscolla03@gmail.com>
2017-09-29 14:45:01 -0300

    jsons produtos e mercados

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //Initialize Express
 const app = express()
+
+//Enable server access within the same place
+app.use(cors())
 
 //Initialize Mongo DB
 // mongoose.connect('mongodb://localhost/kipreco');
@@ -16,10 +20,18 @@ app.use(bodyParser.json())
 // ROUTES
 // Index route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.status(200).send({
+    "email": "renan@mastertech.tech",
+    "pass": "asbasjdkasnjkd"
+  })
+});
+
+app.post('/', (req, res) => {
+  res.sendStatus(200);
+  console.log(`Working`);
+});
 
 // Port config listen
 app.listen(3000, () => {
   console.log('Listening on port 3000!')
-})
+});
